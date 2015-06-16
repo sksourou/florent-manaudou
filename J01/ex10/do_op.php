0a9d400bb08dd4b08204f63cf6828a70692b4b0e
Sofiane KSOUROU <sksourou@e1r10p22.42.fr>
2015-06-16 16:08:11 +0200

    do_OP

---
+++ J01/ex10/do_op.php
@@ -0,0 +1,29 @@
+#!/usr/bin/php
+<?php
+	function ft_split_doop($str)
+	{
+		$str = trim($str);
+		$splited = explode(" ", $str);
+		$splited = array_filter($splited);
+		$splited = array_values($splited);
+		return($splited);
+	}
+	if ($argc == 4)
+	{
+		$str1 = implode(" ", $argv);
+		$tab = ft_split_doop($str1);
+		if ($tab[2] == "-")
+			$result = $tab[1] - $tab[3];
+		else if ($tab[2] == "+")
+			$result = $tab[1] + $tab[3];
+		else if ($tab[2] == "*")
+			$result = $tab[1] * $tab[3];
+		else if ($tab[2] == "/")
+			$result = $tab[1] / $tab[3];
+		else if ($tab[2] == "%")
+			$result = $tab[1] % $tab[3];
+		echo $result."\n";
+	}
+	else
+		echo "Incorrect Parameters\n";
+?>
