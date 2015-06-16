2b7d6e7a8bb4dc6569cff45ff155d5ef4f4d6277
Sofiane KSOUROU <sksourou@e1r10p22.42.fr>
2015-06-16 18:36:48 +0200

    finish him

---
+++ J01/ex12/search_it!.php
@@ -0,0 +1,20 @@
+#!/usr/bin/php
+<?php
+	if ($argv > 0)
+	{
+		$str = $argv[1];
+		foreach($argv as $key => $value)
+		{
+			if ($key > 1)
+			{
+				$tab = explode(":", $value);
+				if ($tab[0] == $str)
+				{
+					$result = $tab[1];
+				}
+
+			}
+		}
+		echo $result;
+	}
+?>
